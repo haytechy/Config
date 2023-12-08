0aa893a753fd21f01a3310ff85a6e3591045ab76
Alex H <alexhayman89@gmail.com>
2023-12-08 14:42:32 -0500

    Added installer

---
+++ install.sh
@@ -0,0 +1,48 @@
+#Install font
+sudo mkdir /usr/share/fonts/JetBrainsMono
+sudo curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip -o /usr/share/fonts/JetBrainsMono/JetBrainsMono.zip
+sudo unzip /usr/share/fonts/JetBrainsMono/JetBrainsMono.zip
+
+#Install Kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+mkdir ~/.local/bin
+mkdir ~/.local/share/applications
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+cp kitty.conf ~/.config/kitty/kitty.conf
+
+#Install zsh and p10k
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i "s|robbyrussell|powerlevel10k/powerlevel10k|" ~/.zshrc
+source ~/.zshrc
+p10k configure
+
+#Configure TMUX
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+#Configure Neovim
+git clone https://github.com/alexhayman/nvim.git ~/.config/nvim
+curl -LO https://github.com/neovim/neovim/releases/download/v0.9.4/nvim.appimage -o ~/Downloads/nvim.appimage
+chmod u+x ~/Downloads/nvim.appimage
+sudo mv ~/Downloads/nvim.appimage /usr/bin/nvim
+
+#Configure Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.zshrc
+nvm install --lts
+nvm use --lts
+
+# bspwmrc and sxhkdrc
+#sudo apt install bspwm
+#install -Dm755 ./bspwmrc ~/.config/bspwm/bspwmrc
+#install -Dm644 sxhkdrc ~/.config/sxhkd/sxhkdrc
+#
+## Installing polybar
+#sudo apt install polybar
+#cp -r polybar ~/.config/polybar
